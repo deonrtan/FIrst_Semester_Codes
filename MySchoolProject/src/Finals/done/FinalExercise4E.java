17cf2f7cec4ccb223f176b2938b59f76793cb6e0
Omori <112740690+deonrtan@users.noreply.github.com>
2022-11-17 05:57:11 +0800

    Bubble Sort and Selection Sort (Strings)

---
+++ MySchoolProject/src/Finals/done/FinalExercise4E.java
@@ -0,0 +1,36 @@
+package Finals.done;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+//Bubble sort algorithm for String, sorting in lexographic ordering (i.e Alphabetical Ordering)
+public class FinalExercise4E {
+
+  static int MAX = 100;
+
+  public static void sortStrings(String[] arr, int n) {
+    String temp;
+
+    // Sorting strings using bubble sort
+    for (int j = 0; j < n - 1; j++) {
+      for (int i = j + 1; i < n; i++) {
+        if (arr[j].compareTo(arr[i]) > 0) {
+          temp = arr[j];
+          arr[j] = arr[i];
+          arr[i] = temp;
+        }
+      }
+    }
+  }
+
+  // Driver code
+  public static void main(String[] args) {
+    String[] arr = { "Zebra", "Anaconda", "Gray", "South", "Window" };
+    int n = arr.length;
+    sortStrings(arr, n);
+    System.out.println("Strings in sorted order are : ");
+    for (int i = 0; i < n; i++) System.out.println(
+      "String " + (i + 1) + " is " + arr[i]
+    );
+  }
+}
---
+++ MySchoolProject/src/Finals/done/FinalExercise4F.java
@@ -0,0 +1,59 @@
+package Finals.done;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+//Selection sort algorithm for String, sorting in lexographic ordering (i.e Alphabetical Ordering)
+public class FinalExercise4F {
+
+  // Sorts an array of strings
+  static void selectionSort(String arr[], int n) {
+    // One by one move boundary of unsorted subarray
+    for (int i = 0; i < n - 1; i++) {
+      // Find the minimum element in unsorted array
+      int min_index = i;
+      String minStr = arr[i];
+      for (int j = i + 1; j < n; j++) {
+        /*compareTo() will return a -ve value,
+            if string1 (arr[j]) is smaller than string2 (minStr)*/
+        // If arr[j] is smaller than minStr
+
+        if (arr[j].compareTo(minStr) < 0) {
+          // Make arr[j] as minStr and update min_idx
+          minStr = arr[j];
+          min_index = j;
+        }
+      }
+
+      // Swapping the minimum element
+      // found with the first element.
+      if (min_index != i) {
+        String temp = arr[min_index];
+        arr[min_index] = arr[i];
+        arr[i] = temp;
+      }
+    }
+  }
+
+  // Driver code
+  public static void main(String args[]) {
+    String arr[] = { "Zebra", "Gray", "Window", "Black", "Data" };
+    int n = arr.length;
+    System.out.println("Given array is");
+
+    // Printing the array before sorting
+    for (int i = 0; i < n; i++) {
+      System.out.println(i + ": " + arr[i]);
+    }
+    System.out.println();
+
+    selectionSort(arr, n);
+
+    System.out.println("Sorted array is");
+
+    // Printing the array after sorting
+    for (int i = 0; i < n; i++) {
+      System.out.println(i + ": " + arr[i]);
+    }
+  }
+}
